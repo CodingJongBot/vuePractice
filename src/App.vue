6c3ac269d2072d99103d881b4422a6745edc8c0f
LeeJongwon <LeeJongwon@MacBook-Pro.local>
2023-12-23 13:00:55 +0900

    learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <template>
-    <h1>
+
+    <h1 @click="increase">
         {{message}}
     </h1>
-    <HelloWorld/>
+    <div v-if="message > 4">
+        4보다 큽니다.
+    </div>
+
+    <ul>
+        <Fruits 
+            v-for="fruit in fruits"
+            :key=fruit
+            :name="fruit">
+            {{fruit}}
+        </Fruits>
+    </ul>
+    <!-- <HelloWorld/> -->
 </template>
 
 
 <script>
 import HelloWorld from '~/components/HelloWorld'
+import Fruits from '~/components/Fruits'
+
+
 export default {
     components:{
-        HelloWorld
+        HelloWorld,
+        Fruits
     },
     data() {
         return{
-            message : "Hello Vue!!!"
+            message : 0,
+            fruits: ['apple','banana','cherry']
         }        
+    },
+    methods:{
+        increase(){
+            this.message+=1
+        }
     }
+
 }
 </script>
+
+
+<style lang="scss">
+    h1 {
+        font-size : 50px;
+        color : deeppink;
+    }
+
+    ul{
+        li{
+            font-size:40px;
+        }
+    }
+
+</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>{{ name }}?!</li>
+    
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            default:''
+        }
+
+        
+    },
+}
+</script>
+
+
+<style scoped lang="scss">
+    h1{
+        color:yellow !important;
+    }
+
+</style>
